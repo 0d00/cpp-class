1ef6c97363c38044a9a18eb2259d57730a22b2d6
0d00 <75377557+0d00@users.noreply.github.com>
2020-12-06 20:09:37 +0800

    Create matrix.h

---
+++ matrix.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class matrix
+{
+private:
+  int memoryNumber;
+   int rows;
+    int columns;
+    float *data;
+public:
+    matrix();
+    matrix(int,int,float*);
+     matrix(int,int);
+     int getRow();
+     int getCol();
+     void setRow(int);
+     void setCol(int);
+     float* getData();
+     void setData(float*);
+    matrix& operator=(matrix) ;
+    matrix operator*(const matrix) const;
+    matrix operator+(matrix) const;
+    matrix operator-(matrix) const;
+    ~matrix();
+    friend ostream& operator<<(ostream&,const matrix&);
+};
+
